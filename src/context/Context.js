f670896d96c0d67e9fb8fadfb03b838921017e75
Tonalli López <aguilablanca_7@live.com>
2020-07-28 21:19:12 -0500

    Fix SSR

--- src/context/Context.js
+++ src/context/Context.js
@@ -5,10 +5,19 @@ import PropTypes from 'prop-types';
 export const Context = createContext();
 
 export const Provider = ({ reducer, children }) => {
-  const initialState = {
-    dark: window.localStorage.getItem('theme') === 'dark' ? true : false,
-    native: window.localStorage.getItem('view') === 'native' ? true : false,
-  };
+  let initialState
+
+  if (typeof window !== 'undefined') {
+    initialState = {
+      dark: localStorage.getItem('theme') === 'dark' ? true : false,
+      native: localStorage.getItem('view') === 'native' ? true : false,
+    };
+  } else {
+    initialState = {
+      dark: false,
+      native: false,
+    };
+  }
 
   return (
     <Context.Provider value={useReducer(reducer, initialState)}>
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,13 +1,13 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'THEME':
-      window.localStorage.setItem('theme', action.payload ? 'dark' : 'light');
+      typeof window !== 'undefined' && localStorage.setItem('theme', action.payload ? 'dark' : 'light');
       return {
         ...state,
         dark: !state.dark,
       };
     case 'NATIVE':
-      window.localStorage.setItem('view', action.payload ? 'native' : 'no-native');
+      typeof window !== 'undefined' && localStorage.setItem('view', action.payload ? 'native' : 'no-native');
       return {
         ...state,
         native: action.payload,
