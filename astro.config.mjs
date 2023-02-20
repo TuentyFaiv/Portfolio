5fb36bf8c0a0639965516a8d0d3acfb90272411d
Tonalli López <aguilablanca_7@live.com>
2023-02-19 18:15:47 -0600

    ⬆️ REFACTOR: Astro to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import lit from "@astrojs/lit";
 import vue from "@astrojs/vue";
 import vercel from "@astrojs/vercel/serverless";
+import lit from "@astrojs/lit";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), lit(), vue()],
+  integrations: [react(), svelte(), vue(), lit()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true
+  })
 });
