088a904968c690943e1e645368e04575c9356044
Tonalli López <contact@tuentyfaiv.com>
2023-08-08 19:04:37 -0600

    🏗️ REFACTOR: Move portfolio to Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  // import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+  import "../theme.postcss";
+
+  import "@skeletonlabs/skeleton/styles/skeleton.css";
+
+  import "../app.postcss";
+  import { AppShell, AppBar, LightSwitch } from "@skeletonlabs/skeleton";
+</script>
+
+<!-- App Shell -->
+<AppShell>
+  <svelte:fragment slot="header">
+    <!-- App Bar -->
+    <AppBar>
+      <svelte:fragment slot="lead">
+        <strong class="text-xl uppercase">Skeleton</strong>
+        <LightSwitch />
+      </svelte:fragment>
+      <svelte:fragment slot="trail">
+        <a
+          class="btn btn-sm variant-ghost-surface"
+          href="https://discord.gg/EXqV7W8MtY"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Discord
+        </a>
+        <a
+          class="btn btn-sm variant-ghost-surface"
+          href="https://twitter.com/SkeletonUI"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          class="btn btn-sm variant-ghost-surface"
+          href="https://github.com/skeletonlabs/skeleton"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <!-- Page Route Content -->
+  <slot />
+</AppShell>
