cf2fb7314a677b8a10142957e5412212f08de781
Tonalli López <contact@tuentyfaiv.com>
2023-09-26 14:07:43 -0600

    🚧 FEAT: ThemeSwitch component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,19 @@
 <script lang="ts">
+  import { modeCurrent } from "@skeletonlabs/skeleton";
+
   import "@fontsource-variable/mulish/wght.css";
   import "@fontsource-variable/mulish/wght-italic.css";
   import "@fontsource-variable/orbitron";
 
   import "@styles";
-  import { AppShell, AppBar, LightSwitch } from "@skeletonlabs/skeleton";
+  import { ThemeSwitch } from "@sharing/atoms";
+
+  const LogoBlack = "/logo_dark@3x.webp";
+  const LogoWhite = "/logo@3x.webp";
 </script>
 
-<AppShell>
-  <svelte:fragment slot="header">
-    <AppBar>
-      <svelte:fragment slot="lead">
-        <strong class="text-xl uppercase">Skeleton</strong>
-        <LightSwitch />
-      </svelte:fragment>
-      <svelte:fragment slot="trail">
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://discord.gg/EXqV7W8MtY"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Discord
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://twitter.com/SkeletonUI"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://github.com/skeletonlabs/skeleton"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </svelte:fragment>
-    </AppBar>
-  </svelte:fragment>
-  <slot />
-</AppShell>
+<svelte:head>
+  <link rel="icon" href={$modeCurrent ? LogoWhite : LogoBlack} />
+</svelte:head>
+<ThemeSwitch />
+<slot />
---
+++ src/ui/sharing/atoms/ThemeSwitch/ThemeSwitch.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import {
+    modeCurrent,
+    setModeUserPrefers,
+    setModeCurrent,
+    getModeOsPrefers,
+    setInitialClassState,
+  } from "@skeletonlabs/skeleton";
+
+  import "./ThemeSwitch.postcss";
+
+  function onToggle() {
+    $modeCurrent = !$modeCurrent;
+    setModeUserPrefers($modeCurrent);
+    setModeCurrent($modeCurrent);
+  }
+
+  onMount(() => {
+    if (!("modeCurrent" in localStorage)) {
+      setModeCurrent(getModeOsPrefers());
+    }
+    setInitialClassState();
+  });
+</script>
+
+<button
+  class="themeswitch"
+  on:click|stopPropagation={onToggle}
+  role="switch"
+  aria-label="Theme switch"
+  aria-checked={$modeCurrent}
+>
+  <span class="themeswitch__litte-stars" />
+  <span class="themeswitch__star" />
+  <span class="themeswitch__clouds" />
+</button>
